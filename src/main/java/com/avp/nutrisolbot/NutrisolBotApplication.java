534904ef554683b14833d944b48a77b55c8c1cf6
VladimirPetranovski <desantluninec@mail.ru>
2021-04-23 09:09:07 +0300

    don't sort out with springcontext

--- src/main/java/com/avp/nutrisolbot/NutrisolBotApplication.java
+++ src/main/java/com/avp/nutrisolbot/NutrisolBotApplication.java
@@ -1,15 +1,15 @@
 package com.avp.nutrisolbot;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
+//import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class NutrisolBotApplication {
 
 	public static void main(String[] args) {
-		ApiContextInitializer.init();
-		SpringApplication.run(NutrisolBotApplication.class, args);
+//		ApiContex.init();
+//		SpringApplication.run(NutrisolBotApplication.class, args);
 	}
 
 }
