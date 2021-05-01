2b92f4c845d2b65088a485d0a42afde036e925d0
VladimirPetranovski <desantluninec@mail.ru>
2021-05-01 23:43:22 +0300

    added in package bean instances Admin, Student

---
+++ src/main/java/com/avp/nutrisolbot/bean/Admin.java
@@ -0,0 +1,28 @@
+package com.avp.nutrisolbot.bean;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+//@Entity
+@Builder
+//@Table(name = "admin")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "Id", nullable = false)
+    private Long id;
+//
+//    @Column(name = "AdminBotState", nullable = false)
+//    @Enumerated(EnumType.ORDINAL)
+//    private AdminBotState adminBotState;
+
+//    @EqualsAndHashCode.Exclude
+//    @ToString.Exclude
+//    @OneToOne(mappedBy = "admin", cascade = CascadeType.ALL)
+    private User user;
+}
---
+++ src/main/java/com/avp/nutrisolbot/bean/Student.java
@@ -0,0 +1,39 @@
+package com.avp.nutrisolbot.bean;
+
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Data
+//@Entity
+@Builder
+//@Table(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
+//@EntityListeners(AuditingEntityListener.class)
+public class Student {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "Id", nullable = false)
+    private Long id;
+
+//    @Column(name = "Firstname")
+    private String firstname;
+
+//    @Column(name = "Surname")
+    private String surname;
+
+//    @Column(name = "Patronymic")
+    private String patronymic;
+
+//    @Column(name = "StudentBotState", nullable = false)
+//    @Enumerated(EnumType.ORDINAL)
+//    private StudentBotState studentBotState;
+
+//    @EqualsAndHashCode.Exclude
+//    @ToString.Exclude
+//    @OneToOne(mappedBy = "student", cascade = CascadeType.ALL)
+    private User user;
+}
