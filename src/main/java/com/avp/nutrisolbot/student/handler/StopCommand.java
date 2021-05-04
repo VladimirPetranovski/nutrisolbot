b1d39aaf83542a8e77d8c1ef26dcbffc8f4addb9
VladimirPetranovski <68049638+VladimirPetranovski@users.noreply.github.com>
2021-05-04 15:41:54 +0300

    Revert "Revert 6 joinning pattern command with handlers"

---
+++ src/main/java/com/avp/nutrisolbot/student/handler/StopCommand.java
@@ -0,0 +1,20 @@
+package com.avp.nutrisolbot.student.handler;
+
+import com.avp.nutrisolbot.service.SendMessageService;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class StopCommand implements Command {
+
+    private final SendMessageService sendMessageService;
+
+    public static final String STOP_MESSAGE = "Деактивировал все ваши подписки \\uD83D\\uDE1F.";
+
+    public StopCommand(SendMessageService sendMessageService) {
+        this.sendMessageService = sendMessageService;
+    }
+
+    @Override
+    public void execute(Update update) {
+        sendMessageService.sendMessage(update.getMessage().getChatId().toString(), STOP_MESSAGE);
+    }
+}
