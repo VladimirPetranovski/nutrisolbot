3c086e47d33d6a0b33ef47283529621a9d6f70bc
VladimirPetranovski <desantluninec@mail.ru>
2021-05-04 19:42:56 +0300

    added StudentInlineKeyboardSource in package Student.buttons, added method getStartAndShareContact

--- src/main/java/com/avp/nutrisolbot/service/SendMessageService.java
+++ src/main/java/com/avp/nutrisolbot/service/SendMessageService.java
@@ -1,6 +1,10 @@
 package com.avp.nutrisolbot.service;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+
 public interface SendMessageService {
 
     void sendMessage(String chatId, String message);
+
+    void sendMessage(String chatId, String message, ReplyKeyboard replyKeyboard);
 }
